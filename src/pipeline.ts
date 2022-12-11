da5d9188d523355880dcaad42751358ed291fa68
David de Boer <david@ddeboer.nl>
2022-12-11 14:33:53 +0100

    Write summaries to file (#19)

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -1,6 +1,7 @@
 import {Analyzer, AnalyzerError, NotSupported} from './analyzer';
 import {SummaryWriter} from './writer';
 import {Selector} from './selector';
+import {Store} from 'n3';
 
 export class Pipeline {
   constructor(
@@ -13,21 +14,27 @@ export class Pipeline {
 
   public async run(): Promise<void> {
     const datasets = await this.config.selector.select();
+    console.info(`Selected ${datasets.size} datasets`);
     for (const dataset of datasets) {
+      console.info(`Analyzing dataset ${dataset.iri}`);
+      const store = new Store();
       for (const analyzer of this.config.analyzers) {
         const result = await analyzer.execute(dataset);
         if (result instanceof NotSupported) {
           console.warn(
-            `Dataset ${dataset.iri} not supported by ${analyzer.constructor.name}`
+            `  ${dataset.iri} not supported by ${analyzer.constructor.name}`
           );
         } else if (result instanceof AnalyzerError) {
           console.warn(
-            `Analysis of ${dataset.iri} failed with message ${result.message}`
+            `  ${dataset.iri} failed with message ${result.message}`
           );
         } else {
           // TODO: add provenance.
-          this.config.writer.write(result);
+          store.addQuads([...result]);
         }
+      }
+      if (store.size > 0) {
+        await this.config.writer.write(dataset, store);
       }
     }
   }
--- src/writer.ts
+++ src/writer.ts
@@ -1,16 +1,25 @@
 import {DatasetCore} from 'rdf-js';
 import {Writer} from 'n3';
+import {writeFile} from 'node:fs/promises';
+import {Dataset} from './dataset';
+import filenamifyUrl from 'filenamify-url';
 
 export interface SummaryWriter {
-  write(dataset: DatasetCore): void;
+  write(dataset: Dataset, summary: DatasetCore): void;
 }
 
 export class FileWriter implements SummaryWriter {
-  write(dataset: DatasetCore): void {
+  write(dataset: Dataset, summary: DatasetCore): void {
     const writer = new Writer();
-    for (const quad of dataset) {
+    for (const quad of summary) {
       writer.addQuad(quad);
     }
-    writer.end((error, result) => console.log(result));
+    writer.end(
+      async (error, result) =>
+        await writeFile(
+          `output/${filenamifyUrl(dataset.iri, {replacement: '-'})}.ttl`,
+          result
+        )
+    );
   }
 }
